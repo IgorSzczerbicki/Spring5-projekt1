35296fe1d041e47c15fbccc3b69f8e0e9d855d0f
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-05 17:46:06 +0200

    @Service
    Warstwa pośrednicząca pomiedzy kontrolerem a @Repository

---
+++ src/com/nauka/spring5/projekt1/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nauka.spring5.projekt1.service.impl;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.nauka.spring5.projekt1.DAO.CustomerDAO;
+import com.nauka.spring5.projekt1.domain.Customer;
+import com.nauka.spring5.projekt1.service.CustomerService;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+}
