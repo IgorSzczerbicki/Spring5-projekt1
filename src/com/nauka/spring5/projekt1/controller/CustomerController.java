a83e8e81d4b5c52f091faed3e5e3708a2b83ca9e
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-05 17:23:23 +0200

    @GetMapping
    Ogranicza sposob przesyłania danych do metody GET

--- src/com/nauka/spring5/projekt1/controller/CustomerController.java
+++ src/com/nauka/spring5/projekt1/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.nauka.spring5.projekt1.DAO.CustomerDAO;
 import com.nauka.spring5.projekt1.domain.Customer;
@@ -16,7 +17,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		List<Customer> customers = customerDAO.getCustomers();
 		model.addAttribute("customers", customers);
